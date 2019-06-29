17a16a1e4e87fc4b6a1e8f02f7e82865cf1b75f3
ywang <ywang@std.uestc.edu.cn>
2019-06-29 11:45:17 +0800

    fixed parameter bug in edsr

--- edsr/utils.py
+++ edsr/utils.py
@@ -4,7 +4,8 @@ import h5py
 import numpy as np
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
- 
+
+
 def read_data(path, Config):
 
     with h5py.File(path, 'r') as hf:
@@ -13,8 +14,9 @@ def read_data(path, Config):
         data_g = np.reshape(data_g, [data_g.shape[0], Config.image_size, Config.image_size, Config.c_dim])
         label_g = np.reshape(label_g, [label_g.shape[0], Config.label_size, Config.label_size, Config.c_dim])
         return data_g, label_g
+
             
-def resBlock(x,channels=64,kernel_size=[3,3],scale=1):
+def resBlock(x,channels=64, kernel_size=[3,3], scale=1):
 
         tmp = slim.conv2d(x,channels,kernel_size,activation_fn=None)
         tmp = tf.nn.relu(tmp)
@@ -22,7 +24,8 @@ def resBlock(x,channels=64,kernel_size=[3,3],scale=1):
         tmp *= scale
         return x + tmp
 
-def upsample(x,scale=2,features=64,activation=tf.nn.relu):
+
+def upsample(x,scale=2, features=64, activation=tf.nn.relu):
 
         assert scale in [2,3,4]
         x = slim.conv2d(x,features,[3,3],activation_fn=activation)
@@ -44,6 +47,7 @@ def upsample(x,scale=2,features=64,activation=tf.nn.relu):
                 x = PS(x,2,color=True)
         return x
 
+
 def _phase_shift(I, r):
 
         bsize, a, b, c = I.get_shape().as_list()
@@ -55,6 +59,7 @@ def _phase_shift(I, r):
         X = tf.split(X, b, 1)  # b, [bsize, a*r, r]
         X = tf.concat([tf.squeeze(x, axis=1) for x in X],2)  # bsize, a*r, b*r
         return tf.reshape(X, (bsize, a*r, b*r, 1))
+
 
 def PS(X, r, color=False):
 
